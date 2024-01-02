0793a8f2d813a00ee1fe04b586fb6aca59257124
KusumotoTakahiro <gantaka921@icloud.com>
2024-01-02 19:44:22 +0900

    [update] admin画面のカスタマイズ＋passwordハッシュ化

--- backend/login/admin.py
+++ backend/login/admin.py
@@ -1,10 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
 from chatgptHandleAPI.models import *
 
 CustomUser = get_user_model()
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+  list_display = (
+    "username",
+    "is_staff",
+    "is_active",
+    "is_admin",
+    "date_joined",
+  )
+  list_filter = (
+    "username",
+    "is_staff",
+    "is_active",
+    "is_admin"
+  )
+  basic = ("username", "password")
+  auth = ("is_staff", "is_active", "is_admin")
+  fieldsets = (
+    ("BasicInfo", {"fields": basic}),
+    ("Auth", {"fields": auth}),
+  )
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Thread)
 admin.site.register(Utterance)
 admin.site.register(UserPersona)
