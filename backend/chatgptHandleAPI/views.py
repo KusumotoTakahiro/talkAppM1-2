21daf7728ab7a93f3ae22653d4d88c4cac25532d
KusumotoTakahiro <gantaka921@icloud.com>
2024-01-11 12:53:45 +0900

    [update] 一旦メッセージがpostできるとこまで

--- backend/chatgptHandleAPI/views.py
+++ backend/chatgptHandleAPI/views.py
@@ -7,43 +7,99 @@ from .serializers import (
   UserPersonaSerializer,
 )
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from functions.response_generator import create_response
 
 
 class ThreadListView(generics.ListCreateAPIView):
   queryset = Thread.objects.all()
-  serializers_class = ThreadSerializer
+  serializer_class = ThreadSerializer
+
+  def create(self, request):
+    data = request.data
+    user_serializer =  self.get_serializer(data=data)
+    user_serializer.is_valid(raise_exception=True)
+    user_serializer.save()
+    headers = self.get_success_headers(user_serializer.data)
+    return Response(
+      user_serializer.data, 
+      status=status.HTTP_201_CREATED, 
+      headers=headers
+    )
+
+
 
 class ThreadDetailView(generics.RetrieveUpdateDestroyAPIView):
   queryset = Thread.objects.all()
-  serializers_class = ThreadSerializer
+  serializer_class = ThreadSerializer
+
 
 
 class UttranceListView(generics.ListCreateAPIView):
   queryset = Utterance.objects.all()
-  serialziers_class = UttranceSerializer
+  serializer_class = UttranceSerializer
+
+  def create(self, request):
+    data = request.data
+    user_serializer =  self.get_serializer(data=data)
+    user_serializer.is_valid(raise_exception=True)
+    user_serializer.save()
+
+    # ここにPOSTデータを加工する処理を追加(ChatGPTを通すなど)
+    processed_data = create_response(data)
+
+    # シリアライザを使って加工後のデータを保存
+    system_serializer = self.get_serializer(data=processed_data)
+    system_serializer.is_valid(raise_exception=True)
+    system_serializer.save()
+
+    headers = self.get_success_headers(user_serializer.data)
+    return Response(
+      system_serializer.data, 
+      status=status.HTTP_201_CREATED, 
+      headers=headers
+    )
+
 
 
 class UttranceDetailView(generics.RetrieveUpdateDestroyAPIView):
   queryset = Utterance.objects.all()
-  serialziers_class = UttranceSerializer
+  serializer_class = UttranceSerializer
+
 
 
 class SystemPersonaListView(generics.ListCreateAPIView):
   queryset = SystemPersona.objects.all()
-  serializers_class = SystemPersonSerializer
+  serializer_class = SystemPersonSerializer
+
+  def create(self, request):
+    data = request.data 
+    user_serializer =  self.get_serializer(data=data)
+    user_serializer.is_valid(raise_exception=True)
+    user_serializer.save()
+
 
 
 class SystemPersonaDetailView(generics.RetrieveUpdateDestroyAPIView):
   queryset = SystemPersona.objects.all()
-  serializers_class = SystemPersonSerializer
+  serializer_class = SystemPersonSerializer
+
 
 
 class UserPersonaListView(generics.ListCreateAPIView):
   queryset = UserPersona.objects.all()
-  serializers_class = UserPersonaSerializer
+  serializer_class = UserPersonaSerializer
+
+  def create(self, request):
+    data = request.data 
+    user_serializer =  self.get_serializer(data=data)
+    user_serializer.is_valid(raise_exception=True)
+    user_serializer.save()
+
 
 
 class UserPersonaDetailView(generics.RetrieveUpdateDestroyAPIView):
   queryset = UserPersona.objects.all()
-  serializers_class = UserPersonaSerializer
+  serializer_class = UserPersonaSerializer
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,11 +2,18 @@
 import * as React from 'react';
 import axios from "axios";
 import { useLocation } from 'react-router-dom';
+import UttranceInput from '../components/uttranceInput'
+import Cataro from '../components/cataro';
+import Grid from '@mui/material/Grid';
 
 
 const Home = () => {
   const location = useLocation()
   const [sessionInfo, setSessionInfo] = React.useState(location.state)
+  const [threadInfo, setThreadInfo] = React.useState(null)
+  const [uttrances, setUttrances] = React.useState(null)
+  const [inputFlag, setInputFlag] = React.useState("0")
+
   React.useEffect(() => {
     // この形で情報が取れる
     axios.get("http://127.0.0.1:8080/account/accounts/", {
@@ -18,10 +25,87 @@ const Home = () => {
     .then(res=>{
       console.log(res);
     })
+    startThread()
   }, [])
+
+  // マウント直後に開始するThread生成関数
+  const startThread = async () => {
+    const baseURL = 'http://127.0.0.1:8080/api/thread'
+    const now = Date.now()
+    axios.post(baseURL, {
+      title: String(now)
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization':'Token '+ sessionInfo.token, 
+      }
+    })
+    .then(res => {
+      const threadInfo = res.data
+      setThreadInfo(threadInfo)
+    })
+  }
+
+  // uttranceInputで使う関数
+  const handleSendMessage = async (message) => {
+    const baseURL = 'http://127.0.0.1:8080/api/Uttrance'
+    try {
+      await axios.post(baseURL, {
+        content: message,
+        talker: 'user',
+        thread: threadInfo.uuid,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization':'Token '+ sessionInfo.token, 
+        }
+      })
+      .then(res => {
+        handleGetMessage()
+        setInputFlag(String(Date.now()))
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  // メッセージの受け取り関数.
+  const handleGetMessage = async () => {
+    const baseURL = 'http://127.0.0.1:8080/api/Uttrance'
+    try {
+      await axios.get(baseURL, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization':'Token '+ sessionInfo.token, 
+        }
+      }).then((res) => {
+        setUttrances(res.data);
+      });
+    }
+    catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
-      {sessionInfo.token}
+    <Grid 
+      container 
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Grid item xs={12} md={12}>
+        <Cataro inputFlag={ inputFlag }/>
+      </Grid>
+      <Grid item xs={12} md={12}>
+        <UttranceInput  onSendMessage={handleSendMessage}/>
+      </Grid>
+    </Grid>
     </>
   );
 };
