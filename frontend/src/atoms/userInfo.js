fd32ea6a72624746c038fcd30167d4831a490102
KusumotoTakahiro <gantaka921@icloud.com>
2024-02-11 15:21:05 +0900

    [add] 雑談スレッドの履歴を管理するページの実装

--- frontend/src/atoms/userInfo.js
+++ frontend/src/atoms/userInfo.js
@@ -1,5 +1,6 @@
 import { atom } from 'recoil'
-
+// recoilでkeyという名前のキーがあるが，単なるUIDみたいなもので使うことはない．
+// 逆にobjectのスネーク記法のキー（session_tokenとか）は実際に識別する際に使う．
 export const userInfo = {
   session_token: atom(
     {
@@ -13,10 +14,18 @@ export const userInfo = {
       default: 0,
     }
   ),
+  // 現在使っているThreadをThreadIDで管理．
   now_thread: atom(
     {
       key: 'nowThread',
       default: '',
     }
   ),
+  // 所持している全てのThreadをThreadIDで管理．
+  own_threads: atom(
+    {
+      key: 'ownThreads',
+      default: [],
+    }
+  )
 }
--- frontend/src/pages/HistoryPage.jsx
+++ frontend/src/pages/HistoryPage.jsx
@@ -1,10 +1,74 @@
+// Pure React
 import * as React from 'react';
+
+// Library
 import axios from 'axios';
+
+// MUI
+import {
+  Box,
+} from '@mui/material';
+
+// Component
+import ThreadHistoryTable from '../components/threadHistoryTable';
+
+// Recoil
+import { useRecoilValue, useRecoilState } from 'recoil';
+import { userInfo } from '../atoms/userInfo';
+
 
 
 const History = () => {
+  const sessionToken = useRecoilValue(userInfo.session_token);
+  const userId = useRecoilValue(userInfo.user_id);
+  const [nowThread, setNowThread] = useRecoilState(userInfo.now_thread);
+  const [ownThreads, setOwnThreads] = useRecoilState(userInfo.own_threads);
+  const baseURL = 'http://127.0.0.1:8080/api';
+
+  React.useEffect(() => {
+    const getThreads = async () => {
+      try {
+        await axios.get(baseURL+'/thread', {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization':'Token '+ sessionToken, 
+          },
+          params: {
+            user : userId,
+          },
+        }).then((res) => {
+          const threads = res.data;
+          setOwnThreads(threads);
+        });
+      }
+      catch (error) {
+        console.log(error);
+      }
+    }
+    getThreads()
+  }, [sessionToken, userId])
+
+  const handleThreadSelect = (event, thread) => {
+    setNowThread(thread.uuid);
+  }
+
   return (
-    <>History</>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        height: '85vh', // 画面の高さいっぱいに要素を配置する場合
+        padding: '30px'
+      }}
+    >
+      <ThreadHistoryTable 
+        threads={ownThreads} 
+        nowThread={nowThread}
+        handleThreadSelect={handleThreadSelect}
+      />
+    </Box>
+    
   )
 }
 
